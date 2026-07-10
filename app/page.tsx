b9aa79502058cd78925c1277a7bcb70ca7c9bcd2
shubhamk010384-blip <shubhamk010384@gmail.com>
2026-07-10 21:53:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Timeline from '../components/Timeline';
+import AwarenessSessions from '../components/AwarenessSessions';
 
-export default function Home(){
- return <>
- <Navbar/>
- <Hero/>
- <Timeline/>
- <Footer/>
- </>
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Timeline />
+      <AwarenessSessions />
+      <Footer />
+    </>
+  );
 }
