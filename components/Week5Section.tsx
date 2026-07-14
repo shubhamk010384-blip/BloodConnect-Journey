6509b1760992d8b5b93be831f5b7b8a90debebbf
shubhamk010384-blip <shubhamk010384@gmail.com>
2026-07-14 22:54:19 +0530

    Update Week5Section.tsx

--- components/Week5Section.tsx
+++ components/Week5Section.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 export default function Week5Section() {
   return (
     <motion.section
+      id="week5"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.7 }}
